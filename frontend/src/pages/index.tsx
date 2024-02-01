b4378d3f12d1b393c5fbcef7358f6636a27e6e18
mathysfrc <mathysf41@gmail.com>
2024-02-01 17:36:45 +0100

    modifs

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,9 +7,8 @@ import isAuth from "@/components/secure/isAuth";
 // } from "@/components/theme/theme";
 import { Button } from "@nextui-org/button";
 
+
 function Home() {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
   return (
     <>
