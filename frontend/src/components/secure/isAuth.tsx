328729b17e17fb52007834ae4efa08f03322aadc
maevacrh <maeva.carinha@icloud.com>
2024-02-01 14:36:36 +0100

    add nextui

--- frontend/src/components/secure/isAuth.tsx
+++ frontend/src/components/secure/isAuth.tsx
@@ -7,21 +7,14 @@ import { useEffect } from "react";
  * If the user is not connected, redirect on the /signin page
  * otherwise display the component as usual.
  */
-export default function isAuth(Component:any) {
+export default function isAuth(Component: any) {
   return function IsAuth(props: any) {
     const router = useRouter();
     const token = localStorage.getItem("token");
 
-    useEffect(() => {
-      if (!token) {
-        router.push('/signin');
-      }
-    }, []);
 
-    if (!token) {
-      return null;
-    }
 
-    return <Component { ...props} />;
+
+    return;
   }
 }
