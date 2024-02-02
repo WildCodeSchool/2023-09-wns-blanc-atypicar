45e1c91daf369b11832117e46e49cc66a11a1b99
c.dochez <c.dochez@criteo.com>
2024-02-02 14:32:59 +0100

    updated NextUI to version 2.2.9

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,50 @@
+// tailwind.config.js
+import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat Alternates", "sans-serif"],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            default: {
+              DEFAULT: "#264653",
+              foreground: "#000000",
+            },
+            primary: {
+              DEFAULT: "#F4A261",
+              foreground: "#000000",
+            },
+            secondary: {
+              DEFAULT: "#E9C46A",
+              foreground: "#000000",
+            },
+            success: {
+              DEFAULT: "#2A9D8F",
+              foreground: "#000000",
+            },
+            danger: {
+              DEFAULT: "#E76F51",
+              foreground: "#000000",
+            },
+          },
+        },
+      },
+    }),
+  ],
+};
+
+export default config;
