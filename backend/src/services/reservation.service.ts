f1693718e67dc1cff10e2ce4d9384732ced9b974
c.dochez <c.dochez@criteo.com>
2024-03-14 17:10:47 +0100

    💄 update journey detail page UI

--- backend/src/services/reservation.service.ts
+++ backend/src/services/reservation.service.ts
@@ -4,94 +4,99 @@ import { Reservation } from "../entities/reservation";
 import { CreateReservationInputType } from "../types/CreateReservationInputType";
 
 export async function searchReservations(): Promise<Reservation[]> {
-    return Reservation.find({
-        relations: {
-            journey: true
-        }
-    });
+  return Reservation.find({
+    relations: {
+      journey: true,
+    },
+  });
 }
 
 export async function findReservation(id: number): Promise<Reservation | null> {
-    return Reservation.findOne({
-        relations: {
-            journey: true
-        },
-        where: { id }
-    })
+  return Reservation.findOne({
+    relations: {
+      journey: true,
+    },
+    where: { id },
+  });
 }
 
-export async function addReservation(ReservationData: CreateReservationInputType, id: number): Promise<Reservation | Error> {
-    try {
-        let journey = await Journey.findOne({
-            relations: {
-                reservation: true,
-                driver: true
-            },
-            where: {
-                id: id
-            }
-        })
+export async function addReservation(
+  ReservationData: CreateReservationInputType,
+  id: number
+): Promise<Reservation | Error> {
+  try {
+    let journey = await Journey.findOne({
+      relations: {
+        reservation: true,
+        driver: true,
+      },
+      where: {
+        id: id,
+      },
+    });
 
-        if (!journey) {
-            throw new Error("Journey not found");
-        } else if (journey.availableSeats === 0) {
-            throw new Error("No seats available");
-        }
-        console.log("journey", journey)
-        if (journey.driver.id === ReservationData.passenger) {
-            throw new Error("You can't book your own journey");
-        }
-        let reservation = new Reservation();
-        reservation.journey = journey;
-        Object.assign(reservation, ReservationData)
-
-        if (journey.availableSeats < reservation.seatNumber) {
-            throw new Error("You booked too much seat, please reduce the number of seats");
-        }
-        journey.availableSeats -= reservation.seatNumber;
-        journey.save();
-
-
-        return reservation.save();
-    } catch (error: any) {
-        return new Error(error.message);
+    if (!journey) {
+      throw new Error("Journey not found");
+    } else if (journey.availableSeats === 0) {
+      throw new Error("No seats available");
     }
+    if (journey.driver.id === ReservationData.passenger) {
+      throw new Error("You can't book your own journey");
+    }
+    let reservation = new Reservation();
+    reservation.journey = journey;
+    Object.assign(reservation, ReservationData);
+
+    if (journey.availableSeats < reservation.seatNumber) {
+      throw new Error(
+        "You booked too much seat, please reduce the number of seats"
+      );
+    }
+    journey.availableSeats -= reservation.seatNumber;
+    journey.save();
+
+    return reservation.save();
+  } catch (error: any) {
+    return new Error(error.message);
+  }
 }
 
-export async function modifyReservation(ReservationData: CreateReservationInputType, id: number): Promise<Reservation | Error> {
-    const reservationToUpdate = await findReservation(id);
+export async function modifyReservation(
+  ReservationData: CreateReservationInputType,
+  id: number
+): Promise<Reservation | Error> {
+  const reservationToUpdate = await findReservation(id);
 
+  if (!reservationToUpdate) {
+    throw new Error("Reservation not found");
+  }
+  // TODO : Rajouter une condition qui permet de bloquer la modification si le nombre de places dispo dans le trajet n'est pas suffisant
+  if (reservationToUpdate.seatNumber < ReservationData.seatNumber) {
+    let diff = reservationToUpdate.seatNumber - ReservationData.seatNumber;
+    reservationToUpdate.journey.availableSeats += diff;
+    reservationToUpdate.journey.save();
+  } else if (reservationToUpdate.seatNumber > ReservationData.seatNumber) {
+    let diff = ReservationData.seatNumber - reservationToUpdate.seatNumber;
+    reservationToUpdate.journey.availableSeats -= diff;
+    reservationToUpdate.journey.save();
+  }
 
-    if (!reservationToUpdate) {
-        throw new Error("Reservation not found")
-    }
-    // TODO : Rajouter une condition qui permet de bloquer la modification si le nombre de places dispo dans le trajet n'est pas suffisant
-    if (reservationToUpdate.seatNumber < ReservationData.seatNumber) {
-        let diff = reservationToUpdate.seatNumber - ReservationData.seatNumber;
-        reservationToUpdate.journey.availableSeats += diff;
-        reservationToUpdate.journey.save();
-    } else if (reservationToUpdate.seatNumber > ReservationData.seatNumber) {
-        let diff = ReservationData.seatNumber - reservationToUpdate.seatNumber;
-        reservationToUpdate.journey.availableSeats -= diff;
-        reservationToUpdate.journey.save();
-    }
+  reservationToUpdate.seatNumber = ReservationData.seatNumber;
 
-
-    reservationToUpdate.seatNumber = ReservationData.seatNumber;
-
-
-    return reservationToUpdate.save();
+  return reservationToUpdate.save();
 }
 
-export async function deleteReservation(id: number): Promise<DeleteResult | string> {
-    const reservation = await findReservation(id);
-    if (!reservation) {
-        return "No reservation find";
-    }
+export async function deleteReservation(
+  id: number
+): Promise<DeleteResult | string> {
+  const reservation = await findReservation(id);
+  if (!reservation) {
+    return "No reservation find";
+  }
 
-    reservation.journey.availableSeats += reservation.seatNumber;
-    await reservation.journey.save();
-    Reservation.delete({ id });
+  reservation.journey.availableSeats += reservation.seatNumber;
+  await reservation.journey.save();
+  Reservation.delete({ id });
 
-    return "Your reservation has been canceled !";
+  return "Your reservation has been canceled !";
 }
