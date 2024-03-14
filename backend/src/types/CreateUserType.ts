446ca78a4d9fbc32d178f15e10c9add90bf959ac
mathysfrc <mathysf41@gmail.com>
2024-03-14 16:09:13 +0100

    View my reservations

--- backend/src/types/CreateUserType.ts
+++ backend/src/types/CreateUserType.ts
@@ -4,35 +4,35 @@ import { InputType, Field } from "type-graphql";
 export class CreateUserType {
     @Field()
     firstName: string;
-  
+
     @Field()
     lastName: string;
 
     @Field({ nullable: true })
     birthday?: Date;
-  
+
     @Field()
     email: string;
-  
+
     @Field()
     password: string;
-  
+
     @Field({ defaultValue: "USER" })
     role: "ADMIN" | "USER";
-  
+
     @Field()
     creationDate: Date;
-  
+
     @Field({ defaultValue: false })
     verifiedLicense: false;
-  
+
     @Field({ defaultValue: false })
     verifiedEmail: false;
-  
+
     @Field({ nullable: true })
     picture: string;
-  
+
     @Field({ nullable: true })
     description: string;
-  
+
 }
