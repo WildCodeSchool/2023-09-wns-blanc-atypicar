a8cc51a205d5e4286615b7b0f7af8c2d3e245b0a
c.dochez <c.dochez@criteo.com>
2024-04-18 10:34:22 +0200

    Fix: fix build config & Docker-compose.prod config

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const dataSource = new DataSource({
   type: "postgres",
   host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_DB as string),
+  port: parseInt(process.env.POSTGRES_PORT as string),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
